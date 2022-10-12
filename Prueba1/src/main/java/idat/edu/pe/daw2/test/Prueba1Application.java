85264dc6c67ca7bfb37aeabc8ddde77ba402f894
PC <PC@LAPTOP-PI2GDMFD>
2022-10-11 22:32:18 -0500

    Commit prueba

---
+++ Prueba1/src/main/java/idat/edu/pe/daw2/test/Prueba1Application.java
@@ -0,0 +1,13 @@
+package idat.edu.pe.daw2.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prueba1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prueba1Application.class, args);
+	}
+
+}
---
+++ Prueba1/src/test/java/idat/edu/pe/daw2/test/Prueba1ApplicationTests.java
@@ -0,0 +1,13 @@
+package idat.edu.pe.daw2.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prueba1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
